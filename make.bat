0c1b262364b441410d7c1580d05905b45a39411f
boldowa <boldowa.q@gmail.com>
2017-08-24 22:29:07 +0900

    make script update

--- make.bat
+++ make.bat
@@ -1,2 +1,8 @@
-g++.exe -static -static-libgcc -static-libstdc++ -o "pixi.exe" -Wall --std=c++11 -Wno-format src/*.cpp src/asar/asardll.c
+@rem -----------------------------------
+@rem --- compile option
+@set CFLAGS=-O2 -s -ffunction-sections -fdata-sections -Wl,--gc-sections
+@rem -----------------------------------
+
+@rem compile
+g++.exe %CFLAGS% -static -static-libgcc -static-libstdc++ -o "pixi.exe" -Wall --std=c++11 -Wno-format src/*.cpp src/asar/asardll.c
 @pause
--- make_debug.bat
+++ make_debug.bat
@@ -1,2 +1,8 @@
-g++.exe -g -static -static-libgcc -static-libstdc++ -o "pixi.exe" -Wall --std=c++11 -Wno-format src/*.cpp src/asar/asardll.c
+@rem -----------------------------------
+@rem --- compile option
+@set CFLAGS=-g -O0
+@rem -----------------------------------
+
+@rem compile
+g++.exe %CFLAGS% -static -static-libgcc -static-libstdc++ -o "pixi.exe" -Wall --std=c++11 -Wno-format src/*.cpp src/asar/asardll.c
 @pause
