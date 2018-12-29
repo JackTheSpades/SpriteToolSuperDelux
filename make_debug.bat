5731a9a2d6e47d766b2e0e5c89dc7292dc8dde2d
tattletalelap <watamudai@gmail.com>
2018-12-29 08:04:30 -0200

    legacy routines cleanup, pixi build bats updated to always build x32 (asar dlls are provided that way), also standard library files removed from the project

--- make_debug.bat
+++ make_debug.bat
@@ -4,5 +4,5 @@
 @rem -----------------------------------
 
 @rem compile
-g++.exe %CFLAGS% -static -static-libgcc -static-libstdc++ -o "pixi.exe" -Wall --std=c++11 -Wno-format src/*.cpp src/asar/asardll.c src/json/base64.cpp -DDEBUGMSG
+g++.exe %CFLAGS% -m32 -static -static-libgcc -static-libstdc++ -o "pixi.exe" -Wall --std=c++11 -Wno-format src/*.cpp src/asar/asardll.c src/json/base64.cpp -DDEBUGMSG
 @pause
