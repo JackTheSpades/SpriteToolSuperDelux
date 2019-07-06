a809bd45b10c0e2c637904eb24b7938053b18c79
Tattletale <46232383+tattletalelap@users.noreply.github.com>
2019-07-06 19:09:52 -0300

    unrelated resources removal
    Sprites, extended, cluster, shooters are now in the project for testing purposes only. I still left the poison mushroom + donut block there. They will be moved elsewhere later. Added a file called removedResources to aid people to back those resources. Minor edits to zip.py and some .gitignore files.

--- asm/Poison.asm
+++ asm/Poison.asm
@@ -4,6 +4,7 @@
 ; This patch will overwrite an existing sprite and turn it into the poison mushroom instead.
 ; Note that you shouldn't reapply this patch to the same ROM after changing !NUM as it will
 ; NOT remove the previous insertion.
+
 ; You can however freely change the other defines and reapply then
 
 !NUM ?= $85			;NORMAL sprite number to insert as
@@ -13,29 +14,49 @@
 
 assert !NUM < $C9
 
-org $01817D+(!NUM*2)	;sprite init pointer
+	!dp = $0000
+	!addr = $0000
+	!bank = $800000
+	!sa1 = 0
+	!gsu = 0
+
+if read1($00FFD6) == $15
+	sfxrom
+	!dp = $6000
+	!addr = !dp
+	!bank = $000000
+	!gsu = 1
+elseif read1($00FFD5) == $23
+	sa1rom
+	!dp = $3000
+	!addr = $6000
+	!bank = $000000
+	!sa1 = 1
+endif
+
+org ($01817D+(!NUM*2))|!bank	;sprite init pointer
 	dw $858B				;power up init
-org $0185CC+(!NUM*2)	;main pointer
+org ($0185CC+(!NUM*2))|!bank	;main pointer
 	dw $C353				;power up routine
 
-org $07F26C+!NUM	;tweaker 1656
+org ($07F26C+!NUM)|!bank	;tweaker 1656
 	db $00
-org $07F335+!NUM	;tweaker 1662
+org ($07F335+!NUM)|!bank	;tweaker 1662
 	db $00
-org $07F3FE+!NUM	;tweaker 166E
+org ($07F3FE+!NUM)|!bank	;tweaker 166E
 	db !Pal-8<<1|!Sec
-org $07F4C7+!NUM	;tweaker 167A
+org ($07F4C7+!NUM)|!bank	;tweaker 167A
 	db $C2
-org $07F590+!NUM	;tweaker 1686
+org ($07F590+!NUM)|!bank	;tweaker 1686
 	db $28
-org $07F659+!NUM	;tweaker 190F
+org ($07F659+!NUM)|!bank	;tweaker 190F
 	db $40
 
 ;handle power up routine
-org $01C4CB
+org $01C4CB|!bank
 	autoclean JML PoisonHurt
 ;handle GFX routine
-org $01C6D6
+org $01C6D6|!bank
 	autoclean JSL PoisonGFX
 	
 freecode							
@@ -44,12 +65,12 @@ freecode
 PoisonHurt:				; code JML's here
 	CMP #$21				; \
 	BNE +					; | retore code for BEQ
-	JML $01C4CF			; /
+	JML $01C4CF|!bank			; /
 +	CMP #!NUM			; \ check if sprite is poison mushoroom
 	BNE +					; /
-	JSL $00F5B7			; \ hurt player 
-	JML $01C57F			; / jump-returns to RTS
-+	JML $01C538			; return to normal power-up code
+	JSL $00F5B7|!bank			; \ hurt player 
+	JML $01C57F|!bank			; / jump-returns to RTS
++	JML $01C538|!bank			; return to normal power-up code
 
 ;input:  A = sprite number - 74
 ;output: A = tile number
