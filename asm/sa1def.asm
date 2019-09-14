29958907748a74e5f48435f5288375e7406e12c0
Erik <eselerik@outlook.com>
2019-09-14 14:38:30 -0500

    fix sa1def.asm a bit

--- asm/sa1def.asm
+++ asm/sa1def.asm
@@ -52,24 +52,38 @@ endmacro
 
 if read1($00FFD5) == $23		; check if the rom is sa-1
 	sa1rom
+	!sa1 = 1
 	!SA1 = 1
 	!SA_1 = 1
 	!Base1 = $3000
 	!Base2 = $6000
+	!dp = $3000
+	!addr = $6000
 	
 	!BankA = $400000
 	!BankB = $000000
+	!bank = $000000
+	
+	!Bank8 = $00
+	!bank8 = $00
 	
 	!SprSize = $16
 else
 	lorom
+	!sa1 = 0
 	!SA1 = 0
 	!SA_1 = 0
 	!Base1 = $0000
 	!Base2 = $0000
-	
+	!dp = $0000
+	!addr = $0000
+
 	!BankA = $7E0000
 	!BankB = $800000
+	!bank = $800000
+	
+	!Bank8 = $80
+	!bank8 = $80
 	
 	!SprSize = $0C
 endif
