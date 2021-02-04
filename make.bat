a8f05aed840a2e748feec97c32a37fa3df9a7370
Alessio <alex.robassomero@gmail.com>
2021-02-04 13:28:14 +0100

    Makefile maintenance
    - For make.bat: 
    -- Removed -ffunction-sections, -fdata-sections and --gc-sections. Reduced binary size by about 100kb. Those flags are not needed and bloat the binary.
    -- Added -Wpedantic and -Wextra for better error checking and confirmation.
    -- Removed Wno-format for the same reason as above.
    - For make_debug.bat
    -- Fixed a very small oversight (removed a single dash).

--- make.bat
+++ make.bat
@@ -1,8 +1,8 @@
 @rem -----------------------------------
 @rem --- compile option
-@set CFLAGS=-O2 -s -ffunction-sections -fdata-sections -Wl,--gc-sections
+@set CFLAGS=-O2 -s
 @rem -----------------------------------
 
 @rem compile
-g++.exe %CFLAGS% -m32 -static -static-libgcc -static-libstdc++ -o "pixi.exe" src/icon.res -Wall --std=c++17 -Wno-format src/*.cpp src/MeiMei/*.cpp src/asar/asardll.c src/json/base64.cpp
+g++.exe %CFLAGS% -m32 -static -static-libgcc -static-libstdc++ -o "pixi.exe" src/icon.res -Wall -Wpedantic -Wextra -std=c++17 src/*.cpp src/MeiMei/*.cpp src/asar/asardll.c src/json/base64.cpp
 @pause
--- make_debug.bat
+++ make_debug.bat
@@ -4,5 +4,5 @@
 @rem -----------------------------------
 
 @rem compile
-g++.exe %CFLAGS% -m32 -static -static-libgcc -static-libstdc++ -o "pixi.exe" src/icon.res -Wall --std=c++17 -Wno-format src/*.cpp src/MeiMei/*.cpp src/asar/asardll.c src/json/base64.cpp -DDEBUGMSG
+g++.exe %CFLAGS% -m32 -static -static-libgcc -static-libstdc++ -o "pixi.exe" src/icon.res -Wall -std=c++17 -Wno-format src/*.cpp src/MeiMei/*.cpp src/asar/asardll.c src/json/base64.cpp -DDEBUGMSG
 @pause
