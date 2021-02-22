8ccb7ad2c863c14fc4aa3dd745acebccb01da42a
Alessio <alex.robassomero@gmail.com>
2021-02-22 13:26:16 +0100

    now compiles on gcc with the correct size.

--- make.bat
+++ make.bat
@@ -4,5 +4,5 @@
 @rem -----------------------------------
 
 @rem compile
-g++.exe %CFLAGS% -m32 -static -static-libgcc -static-libstdc++ -o "pixi.exe" src/icon.rc -Wall -Wpedantic -Wextra -std=c++17 src/*.cpp src/MeiMei/*.cpp src/asar/asardll.c src/json/base64.cpp
+g++.exe %CFLAGS% -m32 -static -static-libgcc -static-libstdc++ -o "pixi.exe" src/icon.res -Wall -Wpedantic -Wextra -std=c++17 src/*.cpp src/MeiMei/*.cpp src/asar/asardll.c src/json/base64.cpp
 @pause
