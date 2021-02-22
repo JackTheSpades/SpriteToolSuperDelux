f3104d464691dd25e8e1451a4cc0a2911b625f48
Alessio <alex.robassomero@gmail.com>
2021-02-22 12:03:27 +0100

    pls

--- build_script.ps1
+++ build_script.ps1
@@ -3,8 +3,9 @@ Set-Location build
 cmake -G "Visual Studio 16 2019" -A Win32 -S ..
 cmake --build . --config Release
 Set-Location ..
-Set-Location "src/CFG Editor/CFG Editor"
-nuget install Newtonsoft.Json -Version 10.0.3
+Set-Location "src/CFG Editor"
+nuget install Newtonsoft.Json -Version 10.0.3 -OutputDirectory packages
+Set-Location "CFG Editor"
 dotnet restore
 msbuild "CFG Editor Project.csproj" -p:Configuration=Release
 Set-Location ..
