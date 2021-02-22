bc6037d0ce0759e8df96a32e1dbb5e7cf209e25f
Alessio <alex.robassomero@gmail.com>
2021-02-22 14:02:42 +0100

    Removed useless files, update gitignore, added README.md file

--- make_debug.bat
+++
@@ -1,8 +0,0 @@
-@rem -----------------------------------
-@rem --- compile option
-@set CFLAGS=-Og -g -ffunction-sections -fdata-sections -Wl,--gc-sections
-@rem -----------------------------------
-
-@rem compile
-g++.exe %CFLAGS% -m32 -static -static-libgcc -static-libstdc++ -o "pixi.exe" src/icon.res -Wall -std=c++17 -Wno-format src/*.cpp src/MeiMei/*.cpp src/asar/asardll.c src/json/base64.cpp -DDEBUGMSG
-@pause
