cacc860750ad43e3885e30510cdf6ef4c0c9152d
Alessio <alex.robassomero@gmail.com>
2022-01-09 16:07:00 +0100

    Update routines.

--- routines/BounceChangeMap16.asm
+++ routines/BounceChangeMap16.asm
@@ -3,34 +3,10 @@
 ;
 ; Input: 
 ;   A = Map16 to change into ($9C)
+;   C = Custom tile flag
 ;
 ; Output:
 ;   N/A
 
 ?main:
-    sta $9C
-    lda !bounce_y_low,x
-    clc
-    adc #$08
-    and #$F0
-    sta $98
-    lda !bounce_y_high,x
-    adc #$00
-    sta $99
-    lda !bounce_x_low,x
-    clc
-    adc #$08
-    and #$F0
-    sta $9A
-    lda !bounce_x_high,x
-    adc #$00
-    sta $9B
-    lda !bounce_properties,x
-    asl
-    rol
-    and #$01
-    sta $1933|!addr
-    phx
-    jsl $00BEB0|!BankB
-    plx
     rtl
