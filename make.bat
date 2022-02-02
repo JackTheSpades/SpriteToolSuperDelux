92a546db8b00e87015fcdc81a5d864115c775845
Alessio <alex.robassomero@gmail.com>
2022-02-02 12:00:07 +0100

    Remove unnecessary files.

--- make.bat
+++
@@ -1,8 +0,0 @@
-@rem -----------------------------------
-@rem --- compile option
-@set CFLAGS=-O2 -s -Wl,--gc-sections
-@rem -----------------------------------
-
-@rem compile
-g++.exe %CFLAGS% -m32 -static -static-libgcc -static-libstdc++ -o "pixi.exe" src/icon.res -Wall -Wpedantic -Wextra -std=c++17 src/*.cpp src/MeiMei/*.cpp src/asar/asardll.c src/json/base64.cpp
-@pause
