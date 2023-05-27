d5fcba66c63710b6dcf6b6ab4aa9de8485e64061
Alessio Rosiello <alex.robassomero@gmail.com>
2023-05-27 21:05:00 +0200

    [NoTest] Update appveyor build scripts.

--- build_script.ps1
+++ build_script.ps1
@@ -10,7 +10,7 @@ Pop-Location
 # build Pixi 64 bit
 mkdir build
 Push-Location build
-cmake -G "Visual Studio 17 2022" -A x64 -S ..
+cmake -DPIXI_BUILD_TESTS=OFF -DPIXI_BUILD_DLL=OFF -DPIXI_BUILD_LIB=OFF -G "Visual Studio 17 2022" -A x64 -S ..
 cmake --build . --config Release --target pixi
 Pop-Location
 
@@ -22,9 +22,10 @@ Remove-Item -Recurse -Force build
 # build Pixi 32 bit
 mkdir build
 Push-Location build
-cmake -G "Visual Studio 17 2022" -A Win32 -S ..
+cmake -DPIXI_BUILD_TESTS=OFF -DPIXI_BUILD_DLL=OFF -DPIXI_BUILD_LIB=OFF -G "Visual Studio 17 2022" -A Win32 -S ..
 cmake --build . --config Release --target pixi
 Pop-Location
 
+py -m pip install mistune bs4
 py .\zip.py
 Rename-Item pixi.zip pixi-x86.zip
--- build_script.sh
+++ build_script.sh
@@ -1,4 +1,7 @@
 mkdir build
 cd build
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -S ..
-make
+cmake -G "Unix Makefiles" -DPIXI_BUILD_TESTS=OFF -DPIXI_BUILD_DLL=OFF -DPIXI_BUILD_LIB=OFF -DCMAKE_BUILD_TYPE=Release -S ..
+make -j $(nproc)
+cd ..
+python3 -m pip install mistune bs4
+python3 zip.py
