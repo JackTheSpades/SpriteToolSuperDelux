4b960d7e8b0d93fafa0412d7a18580f79011fbf3
Alessio Rosiello <alex.robassomero@gmail.com>
2024-03-16 16:30:36 +0100

    Rewrite windows build script a bit

--- build_script.ps1
+++ build_script.ps1
@@ -1,31 +1,35 @@
-# build CFG Editor first
-Push-Location "src/CFG Editor"
-nuget restore "CFG Editor.sln"
-Push-Location "CFG Editor"
-dotnet restore
-msbuild.exe "CFG Editor Project.csproj" -p:Configuration=Release
-Pop-Location
-Pop-Location
+function PackagePixi {
+    param(
+        [string]$Arch = "x64"
+    )
 
-# build Pixi 64 bit
-mkdir build
-Push-Location build
-cmake -DPIXI_BUILD_TESTS=OFF -DPIXI_BUILD_DLL=OFF -DPIXI_BUILD_LIB=OFF -G "Visual Studio 17 2022" -A x64 -S ..
-cmake --build . --config Release --target pixi
-Pop-Location
+    Remove-Item -Recurse -Force build -ErrorAction SilentlyContinue
+
+    # build CFG Editor first
+    Push-Location "src/CFG Editor"
+    Push-Location "CFG Editor"
+    Remove-Item -Recurse -Force "bin/Release/net8.0-windows" -ErrorAction SilentlyContinue
+    dotnet publish --nologo -p:PublishSingleFile=true --sc false -r win-$Arch -c Release -f net8.0-windows -o "bin/Release/net8.0-windows"
+    Pop-Location
+    Pop-Location
+
+    $CmakeArch = "x64"
+    if ($Arch -eq "x86") {
+        $CmakeArch = "Win32"
+    }
+
+    # build pixi
+    mkdir build
+    Push-Location build
+    cmake -DPIXI_BUILD_TESTS=OFF -DPIXI_BUILD_DLL=OFF -DPIXI_BUILD_LIB=OFF -G "Visual Studio 17 2022" -A $CmakeArch -S ..
+    cmake --build . --config Release --target pixi
+    Pop-Location
+
+    # zip
+    py .\zip.py
+    Rename-Item pixi.zip "pixi-windows-$Arch.zip"
+}
 
 py -m pip install mistune bs4
-py .\zip.py
-Rename-Item pixi.zip pixi-windows-x64.zip
-
-Remove-Item -Recurse -Force build
-
-# build Pixi 32 bit
-mkdir build
-Push-Location build
-cmake -DPIXI_BUILD_TESTS=OFF -DPIXI_BUILD_DLL=OFF -DPIXI_BUILD_LIB=OFF -G "Visual Studio 17 2022" -A Win32 -S ..
-cmake --build . --config Release --target pixi
-Pop-Location
-
-py .\zip.py
-Rename-Item pixi.zip pixi-windows-x86.zip
+PackagePixi -Arch "x64"
+PackagePixi -Arch "x86"
